46adbd2741600ff8fa6b022f1649e3b33d6456ee
NikaPapashvili <papashvili.n7@gmail.com>
2025-06-12 22:58:18 +0200

    UPDATE parsing map allow diagonals empty and don't allow tabs

--- sources/map_validation.c
+++ sources/map_validation.c
@@ -7,16 +7,9 @@ static int is_spawn(char c)
     return (0);
 }
 
-static int is_spaces(char c)
-{
-    if (c == ' ' || c == '\t')
-        return (1);
-    return (0);
-}
-
 static int is_whitlisted_char(char c)
 {
-    if (is_spawn(c) || c == '0' || c == '1' || is_spaces(c))
+    if (is_spawn(c) || c == '0' || c == '1' || c == ' ')
         return (1);
     return (0);
 }
@@ -25,13 +18,9 @@ static int is_well_surrounded(char **map, int i, int j)
 {
     if (i == 0 || j == 0 || j == (int)ft_strlen(map[i]) - 1 || !map[i + 1])
         return (-1);
-    if (is_spaces(map[i][j - 1]) || is_spaces(map[i][j + 1]))
+    if (map[i][j - 1] == ' ' || map[i][j + 1] == ' ')
         return (-1);
-    if (is_spaces(map[i + 1][j]) || is_spaces(map[i - 1][j]))
-        return (-1);
-    if (is_spaces(map[i - 1][j - 1]) || is_spaces(map[i - 1][j + 1]))
-        return (-1);
-    if (is_spaces(map[i + 1][j - 1]) || is_spaces(map[i + 1][j + 1]))
+    if (map[i + 1][j] == ' ' || map[i - 1][j] == ' ')
         return (-1);
     return (0);
 }
@@ -42,7 +31,7 @@ static int  is_cell_valid(char **map, int i, int j, int *spawn_found)
         return (-1);
     if (is_spawn(map[i][j]))
         (*spawn_found)++;
-    if (map[i][j] == '1' || is_spaces(map[i][j]))
+    if (map[i][j] == '1' || map[i][j] == ' ')
         return (0);
     if (is_well_surrounded(map, i, j) == -1)
         return (-1);
