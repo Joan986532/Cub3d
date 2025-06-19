d9dd58bdccc7a9faef4c88451e12cb482b35872b
NikaPapashvili <papashvili.n7@gmail.com>
2025-06-19 20:26:25 +0200

    ADD helper comments

--- sources/drawing/draw_overlay.c
+++ sources/drawing/draw_overlay.c
@@ -1,5 +1,11 @@
 #include "cub3d.h"
 
+// MAP: global->map->map
+// PLAYER POS: global->player->pos
+// PLAYER FWD: global->player->fwd
+// CAMERA PLANE: global->player->plane
+// SCREEN WIDHT: WIDTH
+// SCREEN HEIGHT: HEIGHT
 int		draw_overlay(t_mlx_data *data, t_global *global)
 {
     (void)data;
