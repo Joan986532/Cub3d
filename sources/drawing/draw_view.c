84590b39740aef7b48cfa6f416b24217035431bf
NikaPapashvili <papashvili.n7@gmail.com>
2025-06-20 02:18:38 +0200

    UPDATE outside map handling don't draw ceiling nor floor

--- sources/drawing/draw_view.c
+++ sources/drawing/draw_view.c
@@ -3,26 +3,31 @@
 static void	draw_stripe(t_stripe stripe, t_mlx_data *data, t_global *global)
 {
 	int	i;
-	int	start;
-	int	end;
+    int ceiling;
+    int floor;
 
+    ceiling = global->map->ceiling;
+    floor = global->map->floor;
+    if(global->player->pos.x < 0 || global->player->pos.x >= global->map->map_width || global->player->pos.y < 0 || global->player->pos.y >= global->map->map_height)
+    {
+        ceiling = 0x000000;
+        floor = 0x000000;
+    }
 	if (stripe.x < 0 || stripe.x >= WIDTH || stripe.y0 > stripe.y1)
 		return ;
 	if (stripe.y0 < 0)
 		stripe.y0 = 0;
 	if (stripe.y1 >= HEIGHT)
 		stripe.y1 = HEIGHT - 1;
-	start = stripe.y0;
-	end = stripe.y1;
 	i = 0;
 	while (i < HEIGHT)
 	{
-		if (i < start)
-			my_pixel_put(&data->view, stripe.x, i, global->map->ceiling);
-		else if (i <= end)
+		if (i < stripe.y0)
+			my_pixel_put(&data->view, stripe.x, i, ceiling);
+		else if (i >= stripe.y0 && i <= stripe.y1)
 			my_pixel_put(&data->view, stripe.x, i, stripe.color);
 		else
-			my_pixel_put(&data->view, stripe.x, i, global->map->floor);
+			my_pixel_put(&data->view, stripe.x, i, floor);
 		i++;
 	}
 }
@@ -40,8 +45,7 @@ int	draw_view(t_mlx_data *data, t_global *global)
 		calculate_step(&ray, global);
 		perform_dda(&ray, global);
 		calculate_wall_height(&ray);
-		set_wall_color(&ray, global);
-		
+		set_wall_color(&ray, global);		
 		stripe.x = x;
 		stripe.y0 = ray.drawStart;
 		stripe.y1 = ray.drawEnd;
