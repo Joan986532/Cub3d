e21bf46f17d80e199873f20e8eb380fd52531c70
Nika Papashvili <npapashv@1A5.42angouleme.fr>
2025-08-05 02:46:49 +0200

    NORM

--- sources_bonus/header/cub3d_bonus.h
+++ sources_bonus/header/cub3d_bonus.h
@@ -1,5 +1,5 @@
-#ifndef CUB3D_H
-# define CUB3D_H
+#ifndef CUB3D_BONUS_H
+# define CUB3D_BONUS_H
 
 # include "libft.h"
 # include "mlx.h"
@@ -212,61 +212,63 @@ typedef struct s_global
 }		t_global;
 
 /*	PARSING	*/
-int		parsing(char **argv, int argc, t_datamap *map);
-int		iscolor(char *str, t_datamap *map);
-int		istexture(char *str, t_datamap *map);
-void	clear_gnl(char *str, int fd);
-void	clear_textures(t_datamap *map);
-void	ft_strtrime(char *str);
-int		parse_map(char *str, t_datamap *map, int fd);
-void	free_arr(char **arr);
-int		is_valid_map(char **map);
-int		is_spawn(char c);
-int		parsing_extension(char *str, t_datamap *map);
-int		verify_struct(t_datamap *map);
-int		verify_struct_bonus(t_datamap *map);
-char	*get_next_line_no_nl(int fd);
-char	*get_next_line(int fd);
-void	set_spawn(t_player *player, char c, int i, int j);
-void	set_map_dimensions(t_list *linked_map, t_datamap *map);
+int				parsing(char **argv, int argc, t_datamap *map);
+int				iscolor(char *str, t_datamap *map);
+int				istexture(char *str, t_datamap *map);
+void			clear_gnl(char *str, int fd);
+void			clear_textures(t_datamap *map);
+void			ft_strtrime(char *str);
+int				parse_map(char *str, t_datamap *map, int fd);
+void			free_arr(char **arr);
+int				is_valid_map(char **map);
+int				is_spawn(char c);
+int				parsing_extension(char *str, t_datamap *map);
+int				verify_struct(t_datamap *map);
+int				verify_struct_bonus(t_datamap *map);
+char			*get_next_line_no_nl(int fd);
+char			*get_next_line(int fd);
+void			set_spawn(t_player *player, char c, int i, int j);
+void			set_map_dimensions(t_list *linked_map, t_datamap *map);
 
 /*	RENDER	*/
-int		render_frame(void *info);
+int				render_frame(void *info);
 
 /*	DRAWING	*/
-int		draw_minimap(t_datamap *map, t_player *player, t_mlx_data *data);
-int		draw_view(t_mlx_data *data, t_global *global);
-int		draw_overlay(t_mlx_data *data, t_global *global);
-int		draw_sprite(t_mlx_data *data, t_global *global);
+int				draw_minimap(t_datamap *map, t_player *player,
+					t_mlx_data *data);
+int				draw_view(t_mlx_data *data, t_global *global);
+int				draw_overlay(t_mlx_data *data, t_global *global);
+int				draw_sprite(t_mlx_data *data, t_global *global);
 
 /*	RAYCAST	*/
-void	init_ray(t_rat *ray, int x, t_global *global);
-void	calculate_step(t_rat *ray, t_player *player);
-void	calculate_wall_height(t_rat *ray);
-void	set_wall_texture(t_rat *ray, t_global *global);
-void	perform_dda(t_rat *ray, t_global *global);
+void			init_ray(t_rat *ray, int x, t_global *global);
+void			calculate_step(t_rat *ray, t_player *player);
+void			calculate_wall_height(t_rat *ray);
+void			set_wall_texture(t_rat *ray, t_global *global);
+void			perform_dda(t_rat *ray, t_global *global);
 
 /*	STRIPE	*/
-void	calculate_texture_params(t_stripe *stripe, double *tex_pos,
-			double *step);
-void	set_stripe(t_stripe *stripe, t_rat *ray, int x);
+void			calculate_texture_params(t_stripe *stripe,
+					double *tex_pos, double *step);
+void			set_stripe(t_stripe *stripe, t_rat *ray, int x);
 
 /*	ERROR	*/
-int		parsing_error(int code, t_datamap *map);
-int		img_error(int code, t_mlx_img *img);
+int				parsing_error(int code, t_datamap *map);
+int				img_error(int code, t_mlx_img *img);
 
 /*	MINIMAP	*/
-int		init_minimap(t_global *global, t_mlx_data *data, t_datamap *map);
+int				init_minimap(t_global *global, t_mlx_data *data,
+					t_datamap *map);
 
 /*	IMAGE	*/
-void	my_pixel_put(t_mlx_img *img, int x, int y, int color);
-int		init_image(t_mlx_img *img, void *mlx);
-void	bresenham(t_point *point, t_mlx_img *img);
+void			my_pixel_put(t_mlx_img *img, int x, int y, int color);
+int				init_image(t_mlx_img *img, void *mlx);
+void			bresenham(t_point *point, t_mlx_img *img);
 
 /*	TEXTURE	*/
-int		load_textures(t_global *global);
-void	free_textures(t_global *global);
-int		get_texture_color(t_texture *texture, int x, int y);
+int				load_textures(t_global *global);
+void			free_textures(t_global *global);
+int				get_texture_color(t_texture *texture, int x, int y);
 
 /*	CONTROLS	*/
 int				key_press(int keysym, t_global *global);
@@ -275,17 +277,20 @@ int				mouse_moove(int x, int y, t_global *global);
 void			rotate_antitrigo(t_player *player, float angle);
 void			rotate_trigo(t_player *player, float angle);
 int				close_window(void *data);
-unsigned long	gettime_ms();
+unsigned long	gettime_ms(void);
 
 /* DOOR */
-void	handle_door_interact(t_global *global);
-void	update_interactive_cell(t_rat *ray, t_global *global);
-void    reset_interactive_cell(t_global *global);
+void			handle_door_interact(t_global *global);
+void			update_interactive_cell(t_rat *ray, t_global *global);
+void			reset_interactive_cell(t_global *global);
 
 /*	PLAYER	*/
-void	pov_player(t_mlx_data *data, t_player *player, t_datamap *map);
-int		draw_shooting_gun(t_player *player, t_mlx_data *data, t_global *global);
-int		shoot_gun(int button, int x, int y, t_global *global);
-void	set_new_player_position(t_datamap *map, float *x, float *y, float step);
+void			pov_player(t_mlx_data *data, t_player *player,
+					t_datamap *map);
+int				draw_shooting_gun(t_player *player, t_mlx_data *data,
+					t_global *global);
+int				shoot_gun(int button, int x, int y, t_global *global);
+void			set_new_player_position(t_datamap *map, float *x,
+					float *y, float step);
 
 #endif
